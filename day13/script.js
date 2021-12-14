ddb6ce5ccbc9b5b1ec53a520be013be72fb0a2c9
Adrian Marin Megias <74828158+amarin59@users.noreply.github.com>
2021-12-14 17:10:19 +0100

    Update script.js

--- day13/script.js
+++ day13/script.js
@@ -1,6 +1,4 @@
 export default function wrapGifts(gifts) {
-  if(gifts.length === 0) return gifts;
-
   gifts = gifts.map(gift => gift = ('*' + gift + '*'));
   gifts.unshift('*'.repeat(gifts[0].length));
   gifts.push('*'.repeat(gifts[0].length));
