46253fa0d44124dce03572aa019a13f5b1723f86
Adrian Marin Megias <74828158+amarin59@users.noreply.github.com>
2021-12-17 16:07:09 +0100

    Update script.js

--- day17/script.js
+++ day17/script.js
@@ -3,13 +3,10 @@ export default function countPackages(carriers, carrierID) {
   for(let i = 0; i<carriers.length; i++){
     if(carriers[i][0] === carrierID){
       totalPackages += carriers[i][1];
-      if(carriers[i][2].length > 0){
-        for(let j = 0; j<carriers[i][2].length; j++){
-          totalPackages += countPackages(carriers, carriers[i][2][j]);
-        }
+      for(let j = 0; j<carriers[i][2].length; j++){
+        totalPackages += countPackages(carriers.slice(i, carriers.length), carriers[i][2][j]);
       }
     }
   }
-
   return totalPackages;
 }
